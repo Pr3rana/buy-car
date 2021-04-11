1be1de8391fa0167bc0392ef28d7ff8beac32745
Pr3rana <prernasingh216@gmail.com>
2021-04-12 01:47:42 +0530

    Test: Added test cases for default Pagination component.

--- src/components/Pagination/Pageination.js
+++ src/components/Pagination/Pageination.js
@@ -11,12 +11,12 @@ const Pagination = () => {
     },[totalPageCount])
 
     return ( 
-        <div className="pagination-container">
-            <span key="First" onClick = {()=>setPagenumber(1)}>First</span>
-            <span key="Previous" onClick = {()=>setPagenumber(prev=>(prev>1 && prev<=pageCountLimit)?prev-1:prev)}>Previous</span>
-            <span key="Page-count">Page {pagenumber} of {pageCountLimit}</span>
-            <span key="Next" onClick = {()=>setPagenumber(prev=>(prev>=1 && prev<pageCountLimit)?prev+1:prev)}>Next</span>
-            <span key="last" onClick = {()=>setPagenumber(pageCountLimit)}>Last</span>
+        <div  data-testid="pagination-container" className="pagination-container">
+            <span data-testid="first-page" key="First" onClick = {()=>setPagenumber(1)}>First</span>
+            <span data-testid="prev-page" key="Previous" onClick = {()=>setPagenumber(prev=>(prev>1 && prev<=pageCountLimit)?prev-1:prev)}>Previous</span>
+            <span data-testid="curr-total-page" key="Page-count">Page {pagenumber} of {pageCountLimit}</span>
+            <span data-testid="next-page" key="Next" onClick = {()=>setPagenumber(prev=>(prev>=1 && prev<pageCountLimit)?prev+1:prev)}>Next</span>
+            <span data-testid="last-page" key="last" onClick = {()=>setPagenumber(pageCountLimit)}>Last</span>
         </div>
      );
 }
